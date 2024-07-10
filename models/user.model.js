64fe5a4f9bcdda994ce13842e0850e4bd4da222a
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-11 02:28:34 +0300

    feat(models): Updated User schema to include posts
    - Added a new field 'posts' to the userSchema to reference user's posts.
    - The 'posts' field is an array of ObjectIds, referencing the 'post' model.
    
    userSchema fields:
    - fullName: String, required.
    - username: String, unique and required.
    - password: String, minimum length of 7 characters, required.
    - email: String, unique and matching a regex pattern, required.
    - gender: String, required with enum values ['male', 'female', 'other'].
    - avatar: String, default value is defaultAvatar.
    - phone: String, default value is defaultPhone.
    - birthday: String, default value is defaultBirthday.
    - posts: Array of ObjectIds, referencing the 'post' model.
    
    This update allows associating posts with user profiles.

--- models/user.model.js
+++ models/user.model.js
@@ -3,55 +3,68 @@ import { Schema, model } from 'mongoose';
 import handleMongooseError from '../helpers/handleMongooseError.js';
 import addUpdateSettings from '../helpers/addUpdateSettings.js';
 
-import {  required, length, emailRegex, defaultAvatar, defaultPhone, defaultBirthday } from '../utils/constants.js';
+import {
+  required,
+  length,
+  emailRegex,
+  defaultAvatar,
+  defaultPhone,
+  defaultBirthday,
+} from '../utils/constants.js';
 
 const userSchema = new Schema(
-    {
-      fullName: {
-        type: String,
-        required,
-      },
-      username: {
-        type: String,
-        unique: true,
-        required,
-      },
-      password: {
-        type: String,
-        minlength: length(7),
-        required,
-      },
-      email: {
-        type: String,
-        unique: true,
-        match: emailRegex,
-        required,
-      },
-      gender: {
-        type: String,
-        required,
-        enum: ['male', 'female', 'other'],
-      },
-      avatar: {
-        type: String,
-        default: defaultAvatar,
-      },
-      phone: {
-        type: String,
-        default: defaultPhone,
-      },
-      birthday: {
-        type: String,
-        default: defaultBirthday,
-      }
+  {
+    fullName: {
+      type: String,
+      required,
     },
-    { versionKey: false, timestamps: true }
-  );
-  
-  userSchema.post('save', handleMongooseError);
-  userSchema.pre('findOneAndUpdate', addUpdateSettings);
-  userSchema.post('findOneAndUpdate', handleMongooseError);
-  
-  const User = model('User', userSchema);
-  
-  export default User;
+    username: {
+      type: String,
+      unique: true,
+      required,
+    },
+    password: {
+      type: String,
+      minlength: length(7),
+      required,
+    },
+    email: {
+      type: String,
+      unique: true,
+      match: emailRegex,
+      required,
+    },
+    gender: {
+      type: String,
+      required,
+      enum: ['male', 'female', 'other'],
+    },
+    avatar: {
+      type: String,
+      default: defaultAvatar,
+    },
+    phone: {
+      type: String,
+      default: defaultPhone,
+    },
+    birthday: {
+      type: String,
+      default: defaultBirthday,
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'post',
+      },
+    ],
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post('save', handleMongooseError);
+userSchema.pre('findOneAndUpdate', addUpdateSettings);
+userSchema.post('findOneAndUpdate', handleMongooseError);
+
+const User = model('User', userSchema);
+
+export default User;
