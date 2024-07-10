ff9b5aa7bbf20faeb6111d288b13585ba3855aca
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-10 23:52:52 +0300

    feat(tags): Added TAGS_ENUM array with various categories

--- utils/constants.js
+++ utils/constants.js
@@ -1,19 +1,37 @@
 import regExp from './regExp.js';
 
 const required = [true, 'This field is required'];
-const length = lgth => [lgth, `Must be ${lgth} characters or more`];
+const length = (lgth) => [lgth, `Must be ${lgth} characters or more`];
 const emailRegex = [regExp.email, 'Invalid email'];
 const genderEnum = ['male', 'female', 'other'];
 const defaultAvatar = '';
 const defaultPhone = '';
 const defaultBirthday = '';
 
+const TAGS_ENUM = [
+  'culture',
+  'animal',
+  'future',
+  'nature',
+  'fashion',
+  'sport',
+  'politics',
+  'travel',
+  'health',
+  'food',
+  'beauty',
+  'events',
+  'science',
+  'technology',
+];
+
 export {
-    required,
-    length,
-    emailRegex,
-    genderEnum,
-    defaultAvatar,
-    defaultPhone,
-    defaultBirthday
+  required,
+  length,
+  emailRegex,
+  genderEnum,
+  defaultAvatar,
+  defaultPhone,
+  defaultBirthday,
+  TAGS_ENUM,
 };
