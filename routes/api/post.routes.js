37f5d61e3f45ff55c778792ea59f228281d2274a
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-11 23:43:23 +0300

    feat(routes): Added route to retrieve all posts with authentication
    - Imported getPostsCtrl from posts.controller.
    - Defined a new route to retrieve all posts.
    
    New Route:
    - GET /api/posts
      - Protected route that requires user authentication.
      - Uses protectRoute middleware to ensure the user is authenticated.
      - Invokes getPostsCtrl controller to handle fetching all posts.
    
    This update secures the route for retrieving all posts and integrates the getPosts functionality.

--- routes/api/post.routes.js
+++ routes/api/post.routes.js
@@ -3,10 +3,12 @@ import { Router } from 'express';
 import protectRoute from '../../middlewares/protectRoute.js';
 
 import createPostCtrl from '../../controllers/posts.controller/createPost.js';
+import getPostsCtrl from '../../controllers/posts.controller/getPosts.js';
 
 const router = Router();
 
 // private
+router.get('/', protectRoute, getPostsCtrl);
 router.post('/create', protectRoute, createPostCtrl);
 
 export default router;
