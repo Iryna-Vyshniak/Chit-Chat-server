4c9903710c07bb83eb77f61f70e1a94177ec59fe
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-20 15:05:20 +0300

    feat: Added popularPosts to getPosts controller
    - Added a new feature to retrieve and return popular posts along with all posts.
    - The endpoint GET /api/posts now includes both allPosts and popularPosts.
    - Popular posts are sorted by viewsCount and populated with owner details.

--- controllers/posts.controller/getPosts.js
+++ controllers/posts.controller/getPosts.js
@@ -1,25 +1,31 @@
-// @description - GET POSTS
+// @description - GET ALL POSTS
 // @route       - GET /api/posts
-
-import connectToMongoDB from '../../db/connectToMongoDB.js';
 
 import ctrlWrapper from '../../decorators/controllerWrapper.js';
 
 import Post from '../../models/post.model.js';
 
 const getPosts = ctrlWrapper(async (req, res) => {
-  await connectToMongoDB();
-
-  const allPosts = await Post.find().populate('owner', '_id fullName avatar').sort('-createdAt');
-  console.log('allPosts: ', allPosts);
+  const allPosts = await Post.find()
+    .populate('owner', '_id fullName username avatar')
+    .sort('-createdAt');
 
   if (!allPosts.length) {
+    return res.status(200).json({ message: 'There are no posts yet', posts: [] });
+  }
+
+  const popularPosts = await Post.find()
+    .sort('-viewsCount')
+    .populate('owner', '_id username fullName avatar');
+
+  if (!popularPosts) {
     return res.status(200).json({ message: 'There are no posts yet', posts: [] });
   }
 
   return res.status(200).json({
     data: {
       posts: allPosts,
+      popularPosts,
     },
   });
 });
