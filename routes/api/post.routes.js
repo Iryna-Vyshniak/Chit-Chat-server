7801ff4b3fcc3132acd0914fe66bf72102dd10b7
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-21 13:38:17 +0300

    feat: Added routes for getting all tags and posts by tag
    - Added route `GET /api/posts/tags` with `protectRoute` middleware to get all tags
    - Added route `GET /api/posts/tags/:tag` with `protectRoute` middleware to get posts by tag
    - Integrated `getAllTagsCtrl` and `getPostsByTagCtrl` controllers with the routes
    - Ensured routes are protected and require authentication

--- routes/api/post.routes.js
+++ routes/api/post.routes.js
@@ -5,12 +5,16 @@ import protectRoute from '../../middlewares/protectRoute.js';
 import createPostCtrl from '../../controllers/posts.controller/createPost.js';
 import getPostsCtrl from '../../controllers/posts.controller/getPosts.js';
 import getPostCtrl from '../../controllers/posts.controller/getPost.js';
+import getAllTagsCtrl from '../../controllers/posts.controller/getAllTags.js';
+import getPostsByTagCtrl from '../../controllers/posts.controller/getPostsByTag.js';
 
 const router = Router();
 
 // private
 router.get('/', protectRoute, getPostsCtrl);
-router.get('/:id', protectRoute, getPostCtrl);
+router.get('/tags', protectRoute, getAllTagsCtrl);
+router.get('/tags/:tag', protectRoute, getPostsByTagCtrl);
 router.post('/create', protectRoute, createPostCtrl);
+router.get('/:id', protectRoute, getPostCtrl);
 
 export default router;
